1d7cd30553d6055c81e70a3f0e5101f24b35d1d5
Oyku <oykgokcek@gmail.com>
2022-12-26 14:15:18 +0300

    same page is done with js

--- src/index.js
+++ src/index.js
@@ -41,5 +41,85 @@ const siteContent = { // BU NESNEYİ DEĞİŞTİRMEYİN
 
 console.log('Proje açıldı!')
 
+// const anchorArray = document.getElementsByTagName("a").innerHTML;
+// console.log(anchorArray);
 
-/* Kodlar Buradan aşağıya */
+// for(let i =0 ; i<Object.values(siteContent["nav"]).length; i++)
+// {
+//    = Object.values(siteContent["nav"])[i];
+// }
+
+
+document.getElementsByTagName("a")[0].innerHTML = "Servisler";
+document.getElementsByTagName("a")[1].innerHTML = siteContent["nav"]["nav-item-2"];
+document.getElementsByTagName("a")[2].innerHTML = siteContent["nav"]["nav-item-3"];
+document.getElementsByTagName("a")[3].innerHTML = siteContent["nav"]["nav-item-4"];
+document.getElementsByTagName("a")[4].innerHTML = siteContent["nav"]["nav-item-5"];
+document.getElementsByTagName("a")[5].innerHTML = siteContent["nav"]["nav-item-6"];
+
+//neden burda innerHTML kullanmadık??
+
+document.getElementById("logo-img").src = "/mocks/img/logo.png";
+document.getElementById("cta-img").src = "/mocks/img/cta.png";
+document.getElementById("middle-img").src = "/mocks/img/accent.png";
+
+document.getElementsByTagName("h1")[0].textContent = "Bu DOM Mükemmel";
+document.getElementsByTagName("button")[0].textContent = "Başlayın";
+
+
+document.getElementsByClassName("text-content")[0].getElementsByTagName("h4")[0].textContent = "Özellikler";
+
+// document.getElementsByClassName("text-content")[0].getElementsByTagName("p")[0].textContent = 
+// "Özellikler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio 
+// in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+// scelerisque quis.";
+
+document.getElementsByClassName("text-content")[0].getElementsByTagName("p")[0].textContent = 
+siteContent["ana-içerik"]["özellikler-içerik"];
+
+document.getElementsByClassName("text-content")[1].getElementsByTagName("h4")[0].textContent = 
+siteContent["ana-içerik"]["hakkımızda-h4"];
+
+
+document.getElementsByClassName("text-content")[1].getElementsByTagName("p")[0].textContent = 
+siteContent["ana-içerik"]["hakkımızda-içerik"];
+
+
+document.getElementsByClassName("text-content")[2].getElementsByTagName("h4")[0].textContent = 
+siteContent["ana-içerik"]["servisler-h4"];
+
+
+document.getElementsByClassName("text-content")[2].getElementsByTagName("p")[0].textContent = 
+siteContent["ana-içerik"]["servisler-içeriği"];
+
+document.getElementsByClassName("text-content")[3].getElementsByTagName("h4")[0].textContent = 
+siteContent["ana-içerik"]["ürünler-h4"];
+
+
+document.getElementsByClassName("text-content")[3].getElementsByTagName("p")[0].textContent = 
+siteContent["ana-içerik"]["ürünler-içeriği"];
+
+document.getElementsByClassName("text-content")[4].getElementsByTagName("h4")[0].textContent = 
+siteContent["ana-içerik"]["vizyon-h4"];
+
+
+document.getElementsByClassName("text-content")[4].getElementsByTagName("p")[0].textContent = 
+siteContent["ana-içerik"]["vizyon-içeriği"];
+
+
+document.getElementsByClassName("contact")[0].getElementsByTagName("h4")[0].textContent = 
+siteContent["iletisim"]["iletişim-h4"];
+
+
+document.getElementsByClassName("contact")[0].getElementsByTagName("p")[0].textContent = 
+siteContent["iletisim"]["adres"];
+
+
+document.getElementsByClassName("contact")[0].getElementsByTagName("p")[1].textContent = 
+siteContent["iletisim"]["telefon"];
+
+
+document.getElementsByClassName("contact")[0].getElementsByTagName("p")[2].textContent = 
+siteContent["iletisim"]["email"];
+
+document.getElementsByClassName("bold")[0].textContent =  siteContent["footer"]["copyright"];
